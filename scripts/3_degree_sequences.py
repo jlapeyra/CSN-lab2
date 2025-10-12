6a8ab5eaaecc41fdad7e73f27354cf210071a78c
jlapeyra <jlapeyraa@gmail.com>
2025-10-12 22:52:28 +0200

    rename trees->graphs

--- scripts/3_degree_sequences.py
+++ scripts/3_degree_sequences.py
@@ -8,15 +8,15 @@ from collections import Counter
 os.makedirs("data/degree-sequences", exist_ok=True)
 for lang in LANGUAGES:
     print(f'Processing {lang.name}...')
-    src_file = f"data/dependency-trees/{lang.name}.json"
+    src_file = f"data/dependency-graphs/{lang.name}.json"
     dst_file = f"data/degree-sequences/{lang.name}_degree_sequence.txt"
 
     with open(src_file, "r", encoding="utf-8") as f:
-        tree = json.load(f)
+        dependency_graphs = json.load(f)
 
     G = nx.Graph() # Create an undirected graph
-    for i, sentence in enumerate(tree):
-        for child, parent in sentence.items():
+    for i, graph in enumerate(dependency_graphs):
+        for child, parent in graph.items():
             G.add_edge(f'{i}-{child}', f'{i}-{parent}')
     
     with open(dst_file, "w", encoding="utf-8") as f:
